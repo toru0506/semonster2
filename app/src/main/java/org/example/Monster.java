5ba1d50713166fdc260ead9316dd6fe9f5b8d127
m1m23a25 <m1m23a25@oit.ac.jp>
2024-07-08 11:55:13 +0900

    モンスターの追加とモンスターごとのレアリティ固定化

--- app/src/main/java/org/example/Monster.java
+++ app/src/main/java/org/example/Monster.java
@@ -4,16 +4,23 @@ public class Monster {
   private String name;
   private int rare; // 戦うとレア度が高いほうが勝つ．同じ場合は引き分け
   
-  Monster(String name, int rare){
-    this.name = name;
-    this.rare = rare; 
+  Monster(int mnumber){
+    this.name = this.summonMonster(mnumber);
+    this.rare = this.getRare(mnumber); 
   }
 
-  
+  String summonMonster(int mnumber) {
+    String monsters[] = { "スライム", "サハギン", "ドラゴン", "デュラハン", "シーサーペント" };
+    return monsters[mnumber];
+  }
+
+  int getRare(int mnumber) {
+    int rare[] = { 1, 3, 10, 7, 5 };
+    return rare[mnumber];
+  }
 
   @Override
   public String toString() {
-
     return this.name + ":レア度[" + this.rare + "]\n";
   }
 }
--- app/src/test/java/org/example/MonsterTest.java
+++ app/src/test/java/org/example/MonsterTest.java
@@ -5,7 +5,27 @@ import static org.junit.Assert.*;
 
 public class MonsterTest {
     @Test public void appHasAGreeting() {
-        Monster Test1 = new Monster("幽霊",3);
-        assertNotNull("幽霊:レア度[3]" , Test1.toString());
+        Monster Test1 = new Monster(0);
+        assertNotNull("スライム:レア度[1]" , Test1.toString());
+    }
+
+    @Test public void appHasAGreeting2() {
+        Monster Test1 = new Monster(1);
+        assertNotNull("サハギン:レア度[3]" , Test1.toString());
+    }
+
+    @Test public void appHasAGreeting3() {
+        Monster Test1 = new Monster(2);
+        assertNotNull("ドラゴン:レア度[10]" , Test1.toString());
+    }
+
+    @Test public void appHasAGreeting4() {
+        Monster Test1 = new Monster(3);
+        assertNotNull("デュラハン:レア度[7]" , Test1.toString());
+    }
+
+    @Test public void appHasAGreeting5() {
+        Monster Test1 = new Monster(4);
+        assertNotNull("シーサーペント:レア度[5]" , Test1.toString());
     }
 }
