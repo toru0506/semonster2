3e61e01b047c32a8fc2b3c9b485032c85281b5cd
Toru <tftech0506@gmail.com>
2024-07-08 10:06:40 +0900

    App.javaの変更

--- .log/.history/app/src/main/java/org/example/App_20240708093528.java
+++ .log/.history/app/src/main/java/org/example/App_20240708093528.java
@@ -5,7 +5,7 @@ package org.example;
 
 public class App {
     public String getGreeting() {
-        return "こんにちは SEMonster";
+        return "Hello World!";
     }
 
     public static void main(String[] args) {
--- .log/.history/app/src/test/java/org/example/AppTest_20240708100041.java
+++ .log/.history/app/src/test/java/org/example/AppTest_20240708100041.java
@@ -9,6 +9,6 @@ import static org.junit.Assert.*;
 public class AppTest {
     @Test public void appHasAGreeting() {
         App classUnderTest = new App();
-        assertNotNull("こんにちは SEMonster", classUnderTest.getGreeting());
+        assertNotNull("Hello SEMonster", classUnderTest.getGreeting());
     }
 }
